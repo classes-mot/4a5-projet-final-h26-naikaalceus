36de9741b47539b056604fdfdd32ee78e7286db8
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-04-27 13:58:54 -0400

    FAIT: songs.js

---
+++ frontend/src/data/songs.js
@@ -0,0 +1,39 @@
+const SONGS = [
+    {
+        id: "s1",
+        title: "ALL UP IN YOUR MIND",
+        artist: "Beyoncé",
+        album: "Renaissance",
+        releaseYear: "2022",
+    },
+
+    {
+        id: "s2",
+        title: "Outro: Does this make sense?",
+        artist: "BTS",
+        album: "Wild and Dark",
+        releaseYear: "2014",
+    },
+    {
+        id: "s3",
+        title: "I Want You",
+        artist: "Marving Gaye ",
+        album: "I Want You",
+        releaseYear: "1976",
+    },
+    {
+        id: "s4",
+        title: "Shiranaichi",
+        artist: "indigo la End",
+        album: "Crying End Roll",
+        releaseYear: "2017",
+    },
+    {
+        id: "s5",
+        title: "Spit",
+        artist: "Kittie",
+        album: "Spit",
+        releaseYear: "2000",
+    }
+
+]
