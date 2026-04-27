0f1fceca5db4067d1cfbccb3f4ca7737027c4263
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-04-27 15:06:17 -0400

    FAIT: tickets,js et users.js

---
+++ frontend/src/data/users.js
@@ -0,0 +1,23 @@
+export const USERS = [
+    {
+        id: "u1",
+        name: "Onika Burger",
+        image: "",
+        songs: 2,
+        tickets: 1,
+    },
+    {
+        id: "u2",
+        name: "John Pork",
+        image: "",
+        songs: 4,
+        tickets: 2,
+    },
+    {
+        id: "u3",
+        name: "Johny Test",
+        image: "",
+        songs: 5,
+        tickets: 1,
+    },
+];
