678f459fd80cc7ebfc2d005e437e5fd8bb888a55
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-04-30 10:44:35 -0400

    FAIT: ajouts des variables artist, location, date et forfait tickets.js

--- frontend/src/data/tickets.js
+++ frontend/src/data/tickets.js
@@ -1,22 +1,41 @@
 export const tickets = [
     {
         id: "t1",
+        artist: "Megan thee Stallion",
+        location: "Mtl, CA",
+        date: "16 avril 2027",
+        forfait: "De base",
 
     },
     {
         id: "t2",
+        artist: "Beyoncé",
+        location: "Paris, FR",
+        date: "16 avril 2027",
+        forfait: "De base",
 
     },
     {
         id: "t3",
+        artist: "Steve Lacy",
+        location: "LA, USA",
+        date: "6 mai 2027",
+        forfait: "VIP",
 
     },
     {
         id: "t4",
+        artist: "Men I Trust",
+        location: "Mtl, CA",
+        date: "17 juin 2026",
+        forfait: "de base",
 
     },
     {
         id: "t5",
-
+        artist: "Megan thee Stallion",
+        location: "Rio, BR",
+        date: "26 juillet 2027",
+        forfait: "VIP",
     },
 ];
