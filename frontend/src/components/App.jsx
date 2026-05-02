a749cadb9a28a813369959eb9eec851827b1c33c
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-05-02 12:47:59 -0400

    FAIT: App.jsx

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,89 @@
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
+import { useState } from "react";
+import { AuthContext } from "../context/auth-context";
+import RootLayout from "../Containers/Roots";
+import ErrorPage from "../Containers/Roots";
+import Auth from "../Containers/Auth";
+import Subscribe from "../Containers/Subscribe";
+import NewSong from "../Containers/NewSong";
+import NewTicket from "../Containers/NewTicket";
+import UpdateSong from "../Containers/UpdateSong";
+import UpdateTicket from "../Containers/UpdateTicket";
+import Cards from "../Containers/Cards";
+
+const App = () => {
+
+    const storedUserId = sessionStorage.getItem('userId');
+    const storedUserEmail = sessionStorage.getItem('userEmail');
+
+    const [isLoggedIn, setIsLoggedIn] = useState(!storedUserId);
+    const [userId, setUserId] = useState(storedUserId);
+    const [userEmail, setUserEmail] = useState(storedUserEmail);
+
+    const login = (userId, email) => {
+        setIsLoggedIn(true);
+        setUserId(userId);
+        setUserEmail(email);
+        sessionStorage.setItem('userId', userId);
+        sessionStorage.setItem('userEmail', email);
+    };
+
+    const logout = () => {
+        setIsLoggedIn(false);
+        setUserId(null);
+        setUserEmail(null);
+        sessionStorage.removeItem('userId');
+        sessionStorage.removeItem('userEmail');
+    };
+
+    const routerIsLoggedIn = createBrowserRouter([
+        {
+            path: "/",
+            element: <RootLayout />,
+            errorElement: <ErrorPage />,
+            children: [
+                { path: "", element: <Cards /> },
+                { path: "newSong", element: <NewSong /> },
+                { path: "edit/:songId", element: <UpdateSong /> },
+                { path: "newTicket", element: <NewTicket /> },
+                { path: "edit/:ticketId", element: <UpdateTicket /> },
+                { path: "auth", element: <Navigate to="/" replace /> },
+                { path: "subscribe", element: <Navigate to="/" replace /> },
+            ],
+        },
+    ]);
+
+    const routerIsNotLoggedIn = createBrowserRouter([
+        {
+            path: "/",
+            element: <RootLayout />,
+            errorElement: <ErrorPage />,
+            children: [
+                { path: "", element: <Cards /> },
+                { path: "auth", element: <Auth /> },
+                { path: "subscribe", element: <Subscribe /> },
+                { path: "newSong", element: <Navigate to="/auth" replace /> },
+                { path: "edit/:songId", element: <Navigate to="/auth" replace /> },
+                { path: "newTicket", element: <Navigate to="/auth" replace /> },
+                { path: "edit/:ticketId", element: <Navigate to="/auth" replace /> },
+
+            ],
+        },
+    ]);
+
+    const authContextValue = {
+        loggedIn: isLoggedIn,
+        userId: userId,
+        email: userEmail,
+        login: login,
+        logout, logout
+    };
+
+    return (
+        <AuthContext.Provider value={authContextValue}>
+            <RouterProvider router={isLoggedIn ? routerIsLoggedIn : routerIsNotLoggedIn} />;
+        </AuthContext.Provider>
+    )
+}
+
+export default App;
