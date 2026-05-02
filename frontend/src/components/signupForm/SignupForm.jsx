22e435cea6d0907ed5ee93864e6cb2958d483cf1
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-05-02 13:32:15 -0400

    FAIT: ajout css et correction SignUpForm.jsx

--- frontend/src/components/signupForm/SignupForm.jsx
+++ frontend/src/components/signupForm/SignupForm.jsx
@@ -1,6 +1,8 @@
 import { useState, useContext } from "react";
 import { AuthContext } from "../../context/auth-context";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./SignupForm.css";
 
 export default function Signup() {
     const [passwdNotEqual, setPsswdNotEqual] = useState(false);
@@ -23,60 +25,72 @@ export default function Signup() {
         auth.login("ul", data.courriel);
         navigate("/");
     }
-    return {
-        < div >
-        <form onSubmit={handleSubmit}>
-            <h2>Bienvenue !</h2>
-            <p>Veuillez entrer vos informations. </p>
+    return (
+        <div className="container">
+            <form className="form" onSubmit={handleSubmit}>
+                <h2 className="title">Bienvenue !</h2>
+                <p className="subtitle">Veuillez entrer vos informations. </p>
 
-            <div>
-                <label htmlFor="email"> Courriel </label>
-                <input id="email" type="email" name="courriel" required />
-            </div>
-
-            <div>
-                <div>
-                    <label htmlFor="password">Mot de passe</label>
-                    <input id="password" type="password" name="password" />
-                </div>
-                <div>
-                    <label>Confirmez le mot de passe</label>
-                    <input id="confirmer-mdp" type="password" name="confirmer-mdp" required />
-                </div>
-            </div>
-
-            <hr />
-            <div>
-                <div>
-                    <label htmlFor="first-name">Prénom</label>
-                    <input id="first-name" type="text" name="first-name" />
+                <div className="control">
+                    <label htmlFor="email"> Courriel </label>
+                    <input id="email" type="email" name="courriel" required />
                 </div>
 
-                <div>
-                    <label htmlFor="last-name">Nom</label>
-                    <input id="last-name" type="text" name="last-name" />
+                <div className="control-row">
+                    <div className="control">
+                        <label htmlFor="password">Mot de passe</label>
+                        <input id="password" type="password" name="password" />
+                    </div>
+                    <div className="control">
+                        <label>Confirmez le mot de passe</label>
+                        <input
+                            id="confirmer-mdp"
+                            type="password"
+                            name="confirmer-mdp"
+                            required
+                            className={passwdNotEqual ? "input_invalide" : ""}
+                        />
+                    </div>
                 </div>
-            </div>
 
-            <div>
-                <label htmlFor="terms-and-conditions">
-                    <input type="checkbox" id="terns-and-conditions" name="terms" required />
-                    J'accepte les conditions générales de ce  site web
-                </label>
-                <label htmlFor="terms-and-conditions">
-                    <input type="checkbox" id="terns-and-conditions" name="terms" required />
-                    Je souhaite m"inscrire à l'infolettres de ce site web
-                </label>
-            </div>
+                {passwdNotEqual && (
+                    <div className="msg_erreur">
+                        <p>Les mots de passe doivent être pareils ! </p>
+                    </div>
+                )}
+                <hr className="hr_control" />
+                <div className="control_row">
+                    <div className="control">
+                        <label htmlFor="first-name">Prénom</label>
+                        <input id="first-name" type="text" name="first-name" />
+                    </div>
 
-            <div>
-                <p>Vous avez un compte ? Se connecter</p>
-            </div>
+                    <div className="control">
+                        <label htmlFor="last-name">Nom</label>
+                        <input id="last-name" type="text" name="last-name" />
+                    </div>
+                </div>
 
-            <p className="btn_actions">
+                <div className="control checkbox">
+                    <label htmlFor="terms-and-conditions">
+                        <input type="checkbox" id="terns-and-conditions" name="terms" required />
+                        J'accepte les conditions générales de ce  site web
+                    </label>
+                    <label htmlFor="terms-and-conditions">
+                        <input type="checkbox" id="terns-and-conditions" name="terms" required />
+                        Je souhaite m"inscrire à l'infolettres de ce site web
+                    </label>
+                </div>
+
+                <div>
+                    <p className="subtitle">Vous avez un compte ? <Link to="/auth" className="lien">Se connecter</Link></p>
+                </div>
+
+                <p className="btn_actions">
+                    <button type="reset" className="button button_submit">S'inscrire</button>
                     <button type="reset" className="button button_outline">Réinitialiser</button>
                 </p>
-        </form>
+            </form>
         </div >
-    };
+    );
 }
