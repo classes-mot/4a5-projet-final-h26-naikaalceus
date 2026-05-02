db0557cdc4c94f205b32cbcbbd90fa0088333ae1
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-05-02 12:19:44 -0400

    FAIT: ErrorPage.jsx

---
+++ frontend/src/Containers/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import MainNavigation from "../components/Navigation/MainNavigation";
+
+const ErrorPage = () => {
+    return (
+        <>
+            <MainNavigation />
+            <main>
+                <h1>404 - Page introuvable </h1>
+            </main>
+        </>
+    );
+};
+
+export default ErrorPage;
