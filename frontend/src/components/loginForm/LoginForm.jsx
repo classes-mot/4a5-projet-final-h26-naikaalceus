1dc7912e33b2724820529cd3dca5fe59653c8fd8
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-05-02 13:50:21 -0400

    FAIT: ajout du css et correction - LoginForm.jsx

--- frontend/src/components/loginForm/LoginForm.jsx
+++ frontend/src/components/loginForm/LoginForm.jsx
@@ -2,6 +2,7 @@ import { AuthContext } from "../../context/auth-context";
 import { useNavigate } from "react-router-dom";
 import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import "./LoginForm.css";
 
 export default function LoginForm() {
 
@@ -36,32 +37,41 @@ export default function LoginForm() {
         }
     };
     return (
-        <div>
-            <form onSubmit={authSubmitHandler}>
+        <div className="container">
+            <form onSubmit={authSubmitHandler} className="form">
                 <h2>Connexion</h2>
 
-                <div>
-                    <div>
+                <div className="control-row">
+                    <div className="control">
                         <label htmlFor="email">Courriel</label>
-                        <input id="email" tupe="email" name="courriel" />
-                        {emailVide && <div>Le courriel est requis</div>}
+                        <input
+                            id="email"
+                            type="email"
+                            name="courriel"
+                            className={emailVide ? "input_valid" : ""}
+                        />
+                        {emailVide && <div className="msg_erreur">Le courriel est requis</div>}
                     </div>
 
-                    <div>
+                    <div className="control">
                         <label htmlFor="password">Mot de passe</label>
-                        <input id="password" type="password" name="password" />
-                        {mdpVide && <div>Le mot de passe est requis</div>}
+                        <input
+                            id="password"
+                            type="password"
+                            name="password"
+                            className={mdpVide ? "input_valid" : ""}
+                        />
+                        {mdpVide && <div className="msg_erreur">Le mot de passe est requis</div>}
                     </div>
                 </div>
 
                 <div>
-                    <p>Vous n'avez pas de compte ? <Link to="/subscribe">S'inscrice</Link></p>
+                    <p>Vous n'avez pas de compte ? <Link to="/subscribe" className="lien">S'inscrice</Link></p>
                 </div>
 
                 <p>
-                    <button type="reset">Réinitialiser</button>
-                    <button type="submit">Se connecter</button>
-
+                    <button type="submit" className="button button_submit">Se connecter</button>
+                    <button type="reset" className="button button_outline">Réinitialiser</button>
                 </p>
             </form>
         </div>
