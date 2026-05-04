28da4d5944571ba90dcf9c90d6dc9b4a12daf2fb
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-05-04 15:18:42 -0400

    FAIT: complétion tickets-routes.js

--- backend/routes/tickets-routes.js
+++ backend/routes/tickets-routes.js
@@ -1,31 +1,31 @@
 import express from 'express';
 import { check } from 'express-validator';
 import ticketsController from '../controllers/tickets-controllers.js';
-//import checkAuth from '../middleware/check-auth.js';
+import checkAuth from '../middleware/check-auth.js';
 
 const router = express.Router();
 
 // Middleware pour obtenir toutes les tâches
-router.get('/', tasksController.getTasks);
+router.get('/', ticketsController.getTasks);
 
-router.get('/:tid', tasksController.getTasksById);
+router.get('/:tid', ticketsController.getTasksById);
 
-router.get('/user/:uid', tasksController.getTasksByUserId);
+router.get('/user/:uid', ticketsController.getTasksByUserId);
 
 router.use(checkAuth);
 
 router.post(
-  '/',
-  [
-    check('title').not().isEmpty(),
-    check('description').isLength({ min: 10 }),
-    check('dueDate').not().isEmpty(),
-  ],
-  tasksController.createTask
+    '/',
+    [
+        check('artist').not().isEmpty(),
+        check('location').not().isEmpty(),
+        check('date').not().isEmpty(),
+    ],
+    ticketsController.createTask
 );
 
-router.patch('/:tid', tasksController.updateTask);
+router.patch('/:tid', ticketsController.updateTask);
 
-router.delete('/:tid', tasksController.deleteTask);
+router.delete('/:tid', ticketsController.deleteTask);
 
 export default router;
