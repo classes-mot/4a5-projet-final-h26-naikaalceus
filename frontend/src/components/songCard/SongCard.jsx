e0f9dddd1e7f171270b209a8c2216dc911262f34
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-05-04 15:48:55 -0400

    FAIT: SongCard.css et ajout à SongCard.jsx

--- frontend/src/components/songCard/SongCard.jsx
+++ frontend/src/components/songCard/SongCard.jsx
@@ -2,26 +2,27 @@ import Card from "../UIElements/Card";
 import { AuthContext } from "../../context/auth-context";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
+import "./SongCard.css";
 
 const SongCard = (props) => {
     const auth = useContext(AuthContext)
 
     return (
-        <li>
-            <Card>
+        <li className="SongCard">
+            <Card className="SongCard_content">
 
-                <div>
+                <div className="SongCard_info">
                     <h2>{props.title} - {props.artist} </h2>
                     <p>{props.album}</p>
                     <p>{props.releaseYear}</p>
 
                 </div>
 
-                {auth.loggedIn && <div>
+                {auth.loggedIn && <div className="SongCard_actions">
                     <Link to={`/edit/${props.id}`}>
-                        <button>Modifier</button>
+                        <button className="btn_outline">Modifier</button>
                     </Link>
-                    <button onClick={() => props.OnDelete(props.id)}>Supprimer</button>
+                    <button className="button" onClick={() => props.OnDelete(props.id)}>Supprimer</button>
                 </div>
                 }
             </Card>
