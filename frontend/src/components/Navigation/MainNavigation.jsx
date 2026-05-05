b136ec09b0985ca03d80fc8250ce1a1d4cffda39
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-05-05 12:22:06 -0400

    FAIT: importation de SideDrawer.jsx et Backdrop.jsx - MainNavigation.jsx

--- frontend/src/components/Navigation/MainNavigation.jsx
+++ frontend/src/components/Navigation/MainNavigation.jsx
@@ -1,15 +1,35 @@
 import { Link } from "react-router-dom";
-//import { usest } from "react";
+import { useState } from "react";
 import NavLinks from "./NavLinks";
 import "./MainNavigation.css"
+import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop"
 
 const MainNavigation = () => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
+    const openDrawer = () => {
+        setDrawerIsOpen(true);
+    };
+
+    const closeDrawer = () => {
+        setDrawerIsOpen(false);
+    };
 
     return (
         <>
-
+            {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+            {drawerIsOpen && (<SideDrawer onClick={closeDrawer}>
+                <nav>
+                    <NavLinks />
+                </nav>
+            </SideDrawer>)}
             <header className="main-header">
+                <button onClick={openDrawer}>
+                    <span />
+                    <span />
+                    <span />
+                </button>
                 <h1 className="main=navigation__title">
                     <Link to="/">Menu</Link>
                 </h1>
