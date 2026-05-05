f95a3369f7f6899b0d880dc71df99cf33ce7f953
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-05-05 12:03:02 -0400

    FAIT: Correction Menu.jsx

--- frontend/src/components/menu/Menu.jsx
+++ frontend/src/components/menu/Menu.jsx
@@ -1,16 +1,13 @@
 import { Link } from "react-router-dom";
-import "./Menu.css"
+import "./Menu.css";
 
 <>
     <h1 className="title">Bienvenue à toutes et à tous !</h1>
-    <p className="subtitle">En vous inscrivant, vous gagnez accès à une bibliothèque de musique complétement personnalisée. Et encore mieux, vous gagnez aussi accès aux ventes de tickets de vos artistes préférés ! </p>
-
+    <p className="subtitle">En vous inscrivant, vous gagnez accès à une bibliothèque de musique complètement personnalisée. Et encore mieux, vous gagnez aussi accès aux ventes de tickets de vos artistes préférés ! </p>
+    
     <hr className="hr_control" />
     <p className="btn_actions">
-        <button className="button" Link to="/subscribe">Inscrivez-vous</button>
-        <button className="button" Link to="/auth">Connectez-vous</button>
+        <Link to="/subscribe" className="button">Inscrivez-vous</Link>
+        <Link to="/auth" className="button">Connectez-vous</Link>
     </p>
-
-
 </>
-
