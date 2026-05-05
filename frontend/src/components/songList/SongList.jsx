d021a45f707c97d7154b435ded869c41c1788894
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-05-05 18:25:32 -0400

    FAIT: ajout d'une fonction de recherche et mise à jour du css - SongList.jsx et SongList.css

--- frontend/src/components/songList/SongList.jsx
+++ frontend/src/components/songList/SongList.jsx
@@ -3,16 +3,20 @@ import { AuthContext } from "../../context/auth-context";
 import Modal from "../modal/Modal";
 import { useState, useEffect, useContext } from "react";
 import "./SongList.css";
+import SongCard from "../songCard/SongCard"
+
 
 const SongList = (props) => {
 
     const [songs, setSongs] = useState(() => {
         const storedSongs = localStorage.getItem("songs");
-        return (storedSongs && JSON.parse(storedSongs).length > 0) ? JSON.parse(storedSongs) : props.items;
+        return (storedSongs && JSON.parse(storedSongs).length > 0)
+            ? JSON.parse(storedSongs) : props.items;
     });
 
     const [showModal, setShowModal] = useState(false);
     const [idToDelete, setIdToDelete] = useState(null);
+    const [searchTerm, setSearchTerm] = useState("");
     const auth = useContext(AuthContext);
 
     useEffect(() => {
@@ -21,6 +25,10 @@ const SongList = (props) => {
 
     }, [songs]);
 
+    const startDeleteHandler = (id) => {
+        setIdToDelete(id);
+        setShowModal(true);
+    }
     const cancelDeleteHandler = () => {
         setIdToDelete(null);
         setShowModal(false);
@@ -31,6 +39,10 @@ const SongList = (props) => {
         cancelDeleteHandler();
     }
 
+    const filterSongs = songs.filter((song) =>
+        song.title.toLowerCase().includes(searchTerm.toLowerCase)
+    );
+
     return (
         <div className="list_container">
             {showModal && (
@@ -38,9 +50,39 @@ const SongList = (props) => {
                     <p>Êtes-vous sûr de vouloir supprimer cette chanson ? Vous ne pourrez plus l'écouter :(</p>
                 </Modal>
             )}
-            <div>
+
+            <div className="list-header">
+                <input
+                    type="text"
+                    placeholder="Rechercher une chanson. . ."
+                    className="search-bar"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+
                 {auth.loggedIn && (<Link to="/newSong" className="button">Ajouter une chanson</Link>)}
+
             </div>
+
+            {filterSongs.length === 0 ? (
+                <div className="list-center">
+                    <p>Aucune chanson trouvée</p>
+                </div>
+            ) : (
+                <ul className="games_list">
+                    {filterSongs.map((song) => (
+                        <SongCard
+                            key={song.id}
+                            id={song.id}
+                            titre={song.title}
+                            artiste={song.artist}
+                            album={song.album}
+                            anneePublication={song.releaseYear}
+                            OnDelete={startDeleteHandler}
+                        />
+                    ))}
+                </ul>
+            )}
         </div>
     );
 
