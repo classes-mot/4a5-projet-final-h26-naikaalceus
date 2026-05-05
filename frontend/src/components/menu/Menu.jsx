d7244d6a6be8715739247505c5a712232232d57a
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-05-05 15:52:01 -0400

    FAIT: Correction Menu.jsx

--- frontend/src/components/menu/Menu.jsx
+++ frontend/src/components/menu/Menu.jsx
@@ -6,17 +6,7 @@ import "./Menu.css";
 const Menu = () => {
     const auth = useContext(AuthContext)
     return (
-        <>
-
-            <h1 className="title">Bon retour !</h1>
-            <p className="subtitle">Que souhaitez-vous faire ? </p>
-            <p className="btn-actions">
-                <Link to="/newSong" className="button">Consulter vos chanson</Link>
-                <Link to="/newTicket" className="button">Consulter vos tickets</Link>
-            </p>
-
-
-            {!auth.loggedIn && (
+           !auth.loggedIn ? (
                 <>
                     <h1 className="title">Bienvenue à toutes et à tous !</h1>
                     <p className="subtitle">En vous inscrivant, vous gagnez accès à une bibliothèque de musique complètement personnalisée. Et encore mieux, vous gagnez aussi accès aux ventes de tickets de vos artistes préférés ! </p>
@@ -26,9 +16,17 @@ const Menu = () => {
                         <Link to="/auth" className="button">Connectez-vous</Link>
                     </p>
                 </>
-            )}
+                ) : (
 
-        </>
+            <>
+            <h1 className="title">Bon retour !</h1>
+            <p className="subtitle">Que souhaitez-vous faire ? </p>
+            <p className="btn-actions">
+                        <Link to="/newSong" className="button">Consulter vos chanson</Link>
+                        <Link to="/newTicket" className="button">Consulter vos tickets</Link>
+                    </p>
+                    </>         
+          )
     );
 };
 
