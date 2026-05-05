58f27f738f96ebc6db6caa9842dc0022660494c8
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-05-05 16:51:40 -0400

    FAIT: Correction du css - Menu.css et Menu.jsx

--- frontend/src/components/menu/Menu.jsx
+++ frontend/src/components/menu/Menu.jsx
@@ -6,27 +6,38 @@ import "./Menu.css";
 const Menu = () => {
     const auth = useContext(AuthContext)
     return (
-           !auth.loggedIn ? (
-                <>
+        !auth.loggedIn ? (
+            <>
+                <div className="container">
                     <h1 className="title">Bienvenue à toutes et à tous !</h1>
                     <p className="subtitle">En vous inscrivant, vous gagnez accès à une bibliothèque de musique complètement personnalisée. Et encore mieux, vous gagnez aussi accès aux ventes de tickets de vos artistes préférés ! </p>
                     <hr className="hr_control" />
-                    <p className="btn_actions">
-                        <Link to="/subscribe" className="button">Inscrivez-vous</Link>
-                        <Link to="/auth" className="button">Connectez-vous</Link>
-                    </p>
-                </>
-                ) : (
+
+                    <div>
+                        <p className="btn_actions">
+                            <Link to="/subscribe" className="button">Inscrivez-vous</Link>
+                            <Link to="/auth" className="button">Connectez-vous</Link>
+                        </p>
+                    </div>
+                </div>
+            </>
+        ) : (
 
             <>
-            <h1 className="title">Bon retour !</h1>
-            <p className="subtitle">Que souhaitez-vous faire ? </p>
-            <p className="btn-actions">
-                        <Link to="/newSong" className="button">Consulter vos chanson</Link>
-                        <Link to="/newTicket" className="button">Consulter vos tickets</Link>
-                    </p>
-                    </>         
-          )
+                <div className="container">
+                    <h1 className="title">Bon retour !</h1>
+                    <p className="subtitle">Que souhaitez-vous faire ? </p>
+                    <hr className="hr_control" />
+
+                    <div>
+                        <p className="btn-actions">
+                            <Link to="/songs" className="button">Consulter vos chanson</Link>
+                            <Link to="/tickets" className="button">Consulter vos tickets</Link>
+                        </p>
+                    </div>
+                </div>
+            </>
+        )
     );
 };
 
