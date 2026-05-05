0c9bd01fbbd7ca97b6f59a7b80c07da2a244e7b8
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-05-05 15:14:54 -0400

    FAIT: ajout de AuthContext dans Menu.jsx

--- frontend/src/components/menu/Menu.jsx
+++ frontend/src/components/menu/Menu.jsx
@@ -1,13 +1,36 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/auth-context";
 import "./Menu.css";
 
-<>
-    <h1 className="title">Bienvenue à toutes et à tous !</h1>
-    <p className="subtitle">En vous inscrivant, vous gagnez accès à une bibliothèque de musique complètement personnalisée. Et encore mieux, vous gagnez aussi accès aux ventes de tickets de vos artistes préférés ! </p>
-    
-    <hr className="hr_control" />
-    <p className="btn_actions">
-        <Link to="/subscribe" className="button">Inscrivez-vous</Link>
-        <Link to="/auth" className="button">Connectez-vous</Link>
-    </p>
-</>
+const Menu = () => {
+    const auth = useContext(AuthContext)
+    return (
+        <>
+
+            <h1 className="title">Bon retour !</h1>
+            <p className="subtitle">Que souhaitez-vous faire ? </p>
+            <p className="btn-actions">
+                <Link to="/newSong" className="button">Consulter vos chanson</Link>
+                <Link to="/newTicket" className="button">Consulter vos tickets</Link>
+            </p>
+
+
+            {!auth.loggedIn && (
+                <>
+                    <h1 className="title">Bienvenue à toutes et à tous !</h1>
+                    <p className="subtitle">En vous inscrivant, vous gagnez accès à une bibliothèque de musique complètement personnalisée. Et encore mieux, vous gagnez aussi accès aux ventes de tickets de vos artistes préférés ! </p>
+                    <hr className="hr_control" />
+                    <p className="btn_actions">
+                        <Link to="/subscribe" className="button">Inscrivez-vous</Link>
+                        <Link to="/auth" className="button">Connectez-vous</Link>
+                    </p>
+                </>
+            )}
+
+        </>
+    );
+};
+
+export default Menu;
+
