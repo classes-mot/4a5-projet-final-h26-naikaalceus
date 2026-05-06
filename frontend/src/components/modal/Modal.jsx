0bd6d100302fff99a8378e511d2473ca99e7b523
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-05-06 18:43:59 -0400

    NA: correction css - Modal.jsx et Modal.css et correction "song-delete-message" - /fr/translation.json

--- frontend/src/components/modal/Modal.jsx
+++ frontend/src/components/modal/Modal.jsx
@@ -1,11 +1,12 @@
 import { createPortal } from "react-dom";
 import Card from "../UIElements/Card";
 import { useTranslation } from "react-i18next"
+import "./Modal.css"
 
 const Modal = (props) => {
     const { t } = useTranslation();
     const content = (
-        <div className="modal">
+        <div className="modal" onClick={props.onCancel}>
             <Card className="modal_container" onClick={(event) => event.stopPropagation()}>
                 <header className="modal_header">
                     <h2>{props.title || "Confirmation"}</h2>
@@ -13,7 +14,7 @@ const Modal = (props) => {
                 <div className="modal_body">
                     {props.children}
                 </div>
-                <footer modal_footer>
+                <footer className="modal_footer">
                     <button className="btn_cancel" onClick={props.onCancel}>{t('modal.cancel-button')}</button>
                     <button className="btn_confirm" onClick={props.onConfirm}>{t('modal.confirm-button')}</button>
                 </footer>
