47e8a17d5f4b248b7db55fd0b3339819b3cc3c71
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-05-06 10:02:17 -0400

    NA: ajout de la traduction - Menu.jsx

--- frontend/src/components/menu/Menu.jsx
+++ frontend/src/components/menu/Menu.jsx
@@ -2,21 +2,23 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/auth-context";
 import "./Menu.css";
+import { useTranslation } from "react-i18next";
 
 const Menu = () => {
+    const { t, i18n } = useTranslation();
     const auth = useContext(AuthContext)
     return (
         !auth.loggedIn ? (
             <>
                 <div className="container">
-                    <h1 className="title">Bienvenue à toutes et à tous !</h1>
-                    <p className="subtitle">En vous inscrivant, vous gagnez accès à une bibliothèque de musique complètement personnalisée. Et encore mieux, vous gagnez aussi accès aux ventes de tickets de vos artistes préférés ! </p>
+                    <h1 className="title">{t('welcome_NotLoggedIn')}</h1>
+                    <p className="subtitle">{t('menuMessage_NotLoggedIn')}</p>
                     <hr className="hr_control" />
 
                     <div>
                         <p className="btn_actions">
-                            <Link to="/subscribe" className="button">Inscrivez-vous</Link>
-                            <Link to="/auth" className="button">Connectez-vous</Link>
+                            <Link to="/subscribe" className="button">{t('signUp')}</Link>
+                            <Link to="/auth" className="button">{t('login')}</Link>
                         </p>
                     </div>
                 </div>
@@ -25,14 +27,14 @@ const Menu = () => {
 
             <>
                 <div className="container">
-                    <h1 className="title">Bon retour !</h1>
-                    <p className="subtitle">Que souhaitez-vous faire ? </p>
+                    <h1 className="title">{t('welcome_LoggedIn')}</h1>
+                    <p className="subtitle">{t('menuMessage_LoggedIn')}</p>
                     <hr className="hr_control" />
 
                     <div>
                         <p className="btn-actions">
-                            <Link to="/songs" className="button">Consulter vos chanson</Link>
-                            <Link to="/tickets" className="button">Consulter vos tickets</Link>
+                            <Link to="/songs" className="button">{t('viewSongs')}</Link>
+                            <Link to="/tickets" className="button">{t('viewTickets')}</Link>
                         </p>
                     </div>
                 </div>
