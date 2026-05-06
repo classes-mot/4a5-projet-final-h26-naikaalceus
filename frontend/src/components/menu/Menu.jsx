c072e4bb4e81015e6a5e4b4610ae8295fb271fce
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-05-06 16:13:42 -0400

    NA: traduction - Menu.jsx

--- frontend/src/components/menu/Menu.jsx
+++ frontend/src/components/menu/Menu.jsx
@@ -5,20 +5,20 @@ import "./Menu.css";
 import { useTranslation } from "react-i18next";
 
 const Menu = () => {
-    const { t, i18n } = useTranslation();
+    const { t} = useTranslation();
     const auth = useContext(AuthContext)
     return (
         !auth.loggedIn ? (
             <>
                 <div className="container">
-                    <h1 className="title">{t('welcome_NotLoggedIn')}</h1>
-                    <p className="subtitle">{t('menuMessage_NotLoggedIn')}</p>
+                    <h1 className="title">{t('messages.welcome_NotLoggedIn')}</h1>
+                    <p className="subtitle">{t('messages.menuMessage_NotLoggedIn')}</p>
                     <hr className="hr_control" />
 
                     <div>
                         <p className="btn_actions">
-                            <Link to="/subscribe" className="button">{t('signUp')}</Link>
-                            <Link to="/auth" className="button">{t('login')}</Link>
+                            <Link to="/subscribe" className="button">{t('auth.signUp')}</Link>
+                            <Link to="/auth" className="button">{t('auth.login')}</Link>
                         </p>
                     </div>
                 </div>
@@ -27,14 +27,14 @@ const Menu = () => {
 
             <>
                 <div className="container">
-                    <h1 className="title">{t('welcome_LoggedIn')}</h1>
-                    <p className="subtitle">{t('menuMessage_LoggedIn')}</p>
+                    <h1 className="title">{t('messages.welcome_LoggedIn')}</h1>
+                    <p className="subtitle">{t('messages.menuMessage_LoggedIn')}</p>
                     <hr className="hr_control" />
 
                     <div>
                         <p className="btn-actions">
-                            <Link to="/songs" className="button">{t('viewSongs')}</Link>
-                            <Link to="/tickets" className="button">{t('viewTickets')}</Link>
+                            <Link to="/songs" className="button">{t('songs.viewSongs')}</Link>
+                            <Link to="/tickets" className="button">{t('songs.viewTickets')}</Link>
                         </p>
                     </div>
                 </div>
