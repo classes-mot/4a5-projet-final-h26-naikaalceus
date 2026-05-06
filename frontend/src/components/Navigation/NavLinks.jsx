b7bb66e98afe1b9576f626bcbe9471deb1caef0c
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-05-06 12:41:08 -0400

    NA: ajout traduction - NavLinks.jsx et ajout variables "songs" et "tickets" dans les fichiers translation.json

--- frontend/src/components/Navigation/NavLinks.jsx
+++ frontend/src/components/Navigation/NavLinks.jsx
@@ -2,9 +2,10 @@ import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../../context/auth-context";
 import "./NavLinks.css";
+import { useTranslation } from "react-i18next";
 
 const NavLinks = () => {
-
+    const { t } = useTranslation();
     const auth = useContext(AuthContext)
 
     return (
@@ -15,34 +16,34 @@ const NavLinks = () => {
 
             {auth.loggedIn && (
                 <>
-                <li>
-                    <NavLink to="/newSong">Ajouter une chanson</NavLink>
-                </li>
-                <li>
-                        <NavLink to="/newTicket">Ajouter un ticket</NavLink>
+                    <li>
+                        <NavLink to="/newSong">{t('addSong')}</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/songs">Chansons</NavLink>
+                        <NavLink to="/newTicket">{t("addTicket")}</NavLink>
                     </li>
-                     <li>
-                        <NavLink to="/tickets">Tickets</NavLink>
+                    <li>
+                        <NavLink to="/songs">{t('songs')}</NavLink>
                     </li>
-                    </>
+                    <li>
+                        <NavLink to="/tickets">{t('tickets')}</NavLink>
+                    </li>
+                </>
             )}
 
             {!auth.loggedIn ? (
                 <>
                     <li>
-                        <NavLink to="/auth">Se connecter</NavLink>
+                        <NavLink to="/auth">{t('login')}</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/subscribe">S'inscrire</NavLink>
+                        <NavLink to="/subscribe">{t('signUp')}</NavLink>
                     </li>
 
                 </>
             ) : (
                 <li>
-                    <NavLink to="/" onClick={auth.logout}>Déconnexion</NavLink>
+                    <NavLink to="/" onClick={auth.logout}>{t('logout')}</NavLink>
                 </li>
             )}
         </ul>
