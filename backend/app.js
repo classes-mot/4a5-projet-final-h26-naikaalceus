19649a7b41e28c77b7a707bad8e10686c05a362a
Naïka Alcéus <2396941@cmontmorency.qc.ca>
2026-05-06 20:47:35 -0400

    NA: ajout des variables nodemon.json dans app.js

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,15 @@ import { connectDB } from './util/bd.js';
 
 const app = express();
 
+const PORT = process.env.PORT || 5173;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017//bd-deploy';
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); 
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); 
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE'); 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   next();
 });
 
